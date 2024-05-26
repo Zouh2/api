0d02ac595aae5da3c60fb70666ce8812d396a35f
Zouhri anouar <120575295+Zouh2@users.noreply.github.com>
2024-05-26 18:51:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import numpy as np
 import io
 import tensorflow as tf
 import logging
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
